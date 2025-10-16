e7a84564a08a85463ad7106551f3d9c4e68ae8c9
Brandon <brandoncrites76@gmail.com>
2025-10-16 14:05:20 -0700

    apple pay

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/.well-known/apple-developer-merchantid-domain-association',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'text/plain',
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

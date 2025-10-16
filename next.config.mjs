e00ee66af948c0e49ecbdb02758d1e1b77aa82a4
Brandon <brandoncrites76@gmail.com>
2025-10-16 13:44:21 -0700

    Pics and well known

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-developer-merchantid-domain-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
